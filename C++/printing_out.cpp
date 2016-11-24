f00e1a6835f2012ed24afe0ab35f2b3f94f6f7cc
Generic Error <GenericError@users.noreply.github.com>
2016-11-24 11:19:13 +1100

    Added two programs
    Added both the coordinate generation and printing out programs and code

---
+++ C++/printing_out.cpp
@@ -0,0 +1,12 @@
+// C Printing to the command line
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Lorem ipsum dolor sit amet, consectetur adipiscing elit." << '\n';
+  std::cout << "Donec purus ligula, gravida nec dui sit amet, pretium iaculis erat." << '\n';
+  std::cout << "Sed porta turpis sit amet velit ornare sollicitudin." << '\n';
+  std::cout << "Curabitur mollis elit orci, eu eleifend libero sodales vestibulum." << '\n';
+  std::cout << "Nam urna mi, consectetur eu purus et, imperdiet luctus lacus." << '\n';
+  return 0;
+}
