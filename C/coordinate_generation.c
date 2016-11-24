05edd0b5521b49d41441ee5686b5248d3b68af62
Generic Error <GenericError@users.noreply.github.com>
2016-11-24 11:19:37 +1100

    Added two programs
    Added both the coordinate generation and printing out programs and code

---
+++ C/coordinate_generation.c
@@ -0,0 +1,12 @@
+// C Coordinate Generation
+
+int main() {
+  int myArray[500][2] = {};
+  int incrementer = 0;
+  while (incrementer < 501) {
+    myArray[incrementer][0] = incrementer;
+    myArray[incrementer][1] = incrementer * 2;
+    incrementer++;
+  }
+  return 0;
+}
---
+++ C/printing_out.c
@@ -0,0 +1,12 @@
+// C Printing to the command line
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n");
+  printf("Donec purus ligula, gravida nec dui sit amet, pretium iaculis erat.\n");
+  printf("Sed porta turpis sit amet velit ornare sollicitudin.\n");
+  printf("Curabitur mollis elit orci, eu eleifend libero sodales vestibulum.\n");
+  printf("Nam urna mi, consectetur eu purus et, imperdiet luctus lacus.\n");
+  return 0;
+}
