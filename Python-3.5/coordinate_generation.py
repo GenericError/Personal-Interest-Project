deaa2de3ad3494489d8eac08705c0fc6b10499e4
Generic Error <GenericError@users.noreply.github.com>
2016-11-24 11:20:27 +1100

    Added two programs
    Added both the coordinate generation and printing out programs and code

---
+++ Python-3.5/coordinate_generation.py
@@ -0,0 +1,7 @@
+# Python 3.5 Coordinate Generation
+
+MYARRAY = []
+INCREMENTER = 0
+while INCREMENTER < 501:
+    MYARRAY.append([INCREMENTER, INCREMENTER*2])
+    INCREMENTER += 1
---
+++ Python-3.5/printing_out.py
@@ -0,0 +1,7 @@
+# Python 3.5 Printing to the command line
+
+print("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+print("Donec purus ligula, gravida nec dui sit amet, pretium iaculis erat.")
+print("Sed porta turpis sit amet velit ornare sollicitudin.")
+print("Curabitur mollis elit orci, eu eleifend libero sodales vestibulum.")
+print("Nam urna mi, consectetur eu purus et, imperdiet luctus lacus.")
